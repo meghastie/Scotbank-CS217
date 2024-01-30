fab7d5fdd616605593983e68b5a51a622f0079bd
Kath <kathryn.wildsmith.2020@uni.strath.ac.uk>
2024-01-30 22:49:03 +0000

    changed account to work with bigDecimal

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -3,39 +3,49 @@ package uk.co.asepstrath.bank;
 import java.math.BigDecimal;
 
 public class Account {
-    private double value;
+   // private double value;
+    private BigDecimal dec;
     private String name;
 
     public Account(String n, double amount) {
-        value = amount;
+        //value = amount;
         name = n;
+        dec = BigDecimal.valueOf(amount);
     }
     public String getName(){ return name; }
 
+    public String toString(){
+        return name + " - " + dec.toString();
+    }
+
 
     public void deposit(double amount){
-        value = value*100;
+       /* value = value*100;
         value = value+(amount*100);
         value = value/100.0;
+    */
+        dec = dec.add(BigDecimal.valueOf(amount));
 
 
     }
 
     public double getBalance() {
-        return value;
+        //return value;
+        return dec.doubleValue();
     }
 
-    public double withdraw(double money){
-        try{value = value*100;
+    public void withdraw(double money){
+        /* value = value*100;
             value = value-(money*100);
             value = value/100.0;
             if(value<0){
                 throw new ArithmeticException();
             }
-        } catch(ArithmeticException MyException) {
-            System.out.println("Not enough money!");
+        */
+        dec = dec.subtract(BigDecimal.valueOf(money));
+        if(dec.doubleValue()<0){
+            throw new ArithmeticException();
         }
-        return money;
     }
 
 }
