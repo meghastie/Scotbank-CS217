7b0d951b8dad246d4944c02ec16f13f3474b944f
yyb20158 <kathryn.wildsmith.2020@uni.strath.ac.uk>
2024-02-06 13:10:47 +0000

    merge fix

--- src/main/java/uk/co/asepstrath/bank/Account.java
+++ src/main/java/uk/co/asepstrath/bank/Account.java
@@ -3,39 +3,77 @@ package uk.co.asepstrath.bank;
 import java.math.BigDecimal;
 
 public class Account {
-    private double value;
-    private String name;
 
-    public Account(String n, double amount) {
-        value = amount;
-        name = n;
+    private BigDecimal dec;
+    private String name;
+    private String accNum;
+    private String sortCode;
+    private String cardNumber;
+    private String cvc;
+    private String username;
+    private String password;
+
+    public Account(String nme, String acc, String sort, String card, String cv,  String user, String pass, int amount) {
+        name = nme;
+        setAccountNumber(acc);
+        setCardNumber(card);
+        setCVC(cv);
+        dec = BigDecimal.valueOf(amount);
     }
     public String getName(){ return name; }
 
+    public String toString(){
+        return name + " - " + dec.toString();
+    }
+
 
     public void deposit(double amount){
-        value = value*100;
-        value = value+(amount*100);
-        value = value/100.0;
-
-
+        dec = dec.add(BigDecimal.valueOf(amount));
     }
 
     public double getBalance() {
-        return value;
+        return dec.doubleValue();
     }
 
-    public double withdraw(double money){
-        try{value = value*100;
-            value = value-(money*100);
-            value = value/100.0;
-            if(value<0){
-                throw new ArithmeticException();
-            }
-        } catch(ArithmeticException MyException) {
-            System.out.println("Not enough money!");
+    public void withdraw(double money){
+        dec = dec.subtract(BigDecimal.valueOf(money));
+        if(dec.doubleValue()<0){
+            throw new ArithmeticException();
         }
-        return money;
+    }
+    public String getAccNum(){ return accNum; }
+    public String getSortCode(){ return sortCode; }
+    public String getCardNumber(){ return cardNumber; }
+    public String getCvc(){ return cvc; }
+    public String getUsername(){ return username; }
+    public boolean checkPassword(String guess){
+        return guess.equals(password);
     }
 
+    public void setName(String nm){ name = nm;}
+    public void setAccountNumber(String acc){
+        if(acc.length() != 8){
+            System.out.println("Account  Number error");
+        }
+        else{
+            accNum = acc;
+        }
+    }
+    public void setCVC(String CVC){
+        if(CVC.length() != 3){
+            System.out.print("CVC error");
+        }
+        else{
+            cvc = CVC;
+        }
+    }
+    public void setCardNumber(String card){
+        if(card.length() != 16){
+            System.out.println("card number error");
+        }
+        else{
+            cardNumber = card;
+        }
+    }
 }
+
