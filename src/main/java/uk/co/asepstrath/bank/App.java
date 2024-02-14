e9489dfff83840340e8affd2ac305944605c9af7
luke <luke.morrison.2022@uni.strath.ac.uk>
2024-02-14 16:51:48 +0000

    Changes made to transaction class

--- src/main/java/uk/co/asepstrath/bank/App.java
+++ src/main/java/uk/co/asepstrath/bank/App.java
@@ -8,9 +8,11 @@ import io.jooby.hikari.HikariModule;
 import org.slf4j.Logger;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class App extends Jooby {
 
@@ -21,6 +23,7 @@ public class App extends Jooby {
         install(new UniRestExtension());
         install(new HandlebarsModule());
         install(new HikariModule("mem"));
+
 
         /*
         This will host any files in src/main/resources/assets on <host>/assets
@@ -35,7 +38,8 @@ public class App extends Jooby {
         DataSource ds = require(DataSource.class);
         Logger log = getLog();
 
-        mvc(new ExampleController(ds,log));
+
+        mvc(new ControllerTest(ds,log));
 
         /*
         Finally we register our application lifecycle methods
@@ -44,9 +48,7 @@ public class App extends Jooby {
         onStop(() -> onStop());
     }
 
-    public static void main(final String[] args) {
-        runApp(args, App::new);
-    }
+    public static void main(final String[] args) {runApp(args, App::new);}
 
     /*
     This function will be called when the application starts up,
@@ -61,19 +63,76 @@ public class App extends Jooby {
         // Open Connection to DB
         try (Connection connection = ds.getConnection()) {
             //
+            java.util.Date currentDate = new java.util.Date();
+            Date sqlDate = new Date(currentDate.getTime());
             Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE `Example` (`Key` varchar(255),`Value` varchar(255))");
-            stmt.executeUpdate("INSERT INTO Example " + "VALUES ('WelcomeMessage', 'Welcome to A Bank')");
+            stmt.executeUpdate("CREATE TABLE IF NOT EXSITS `Customer` (\n"
+                    + "`name` varchar(255) NOT NULL \n"
+                    + "`username` varchar(255) PRIMARY KEY \n" //two users cannot have same username, however they could possibly have same accNo - only unique identifier when paired with sort code
+                    + "`passcode` varchar(255) NOT NULL \n"
+                    + ")");
+
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXSITS `Account` (\n"
+                    + "`AccountId` varchar(50) NOT NULL, \n"
+                    + "`customerName` varchar(100) NOT NULL, \n"
+                    + "`AccountType` varchar(45) NOT NULL \n"
+                    + "`startingbalance` double NOT NULL \n"
+                    + "`RoundUpEnabled` varchar(10) NOT NULL \n"
+                    + "PRIMARY KEY (`AccountId`)," //users may have the same account no OR sort code, but never 2 customers with the same acc no AND sort code
+                    + "FOREIGN KEY (`customerName`) REFERENCES `Customer`(`customerName`)"
+                    + ")");
+
+            stmt.executeUpdate("CREATE TABLE IF NOT EXSITS `Transaction` (\n"
+                    + "`transactionID` integer NOT NUll"
+                    + "`Type` varchar(15) NOT NULL, \n"
+                    + "`amount` double NOT NULL, \n"
+                    + "`to` varchar(50) \n"
+                    + "`from` varchar(50) integer  \n"
+                    + "`transactionID` integer PRIMARY KEY \n" //needed to uniquley identify the transaction as users can have many
+                    + ")");
+
+            //continue with next part - inserting data in 'Agile_Lab_Doc'
+            String insert = ("INSERT INTO Customer(name, username, password, dob)"
+                    + "VALUES (?,?,?,?)");
+            PreparedStatement prep = connection.prepareStatement(insert);
+            prep.setString(1, "Bob");
+            prep.setString(2,"bOB2024");
+            prep.setString(3, "OnlineBanking1234*");
+            prep.setDate(4, sqlDate); //dob is current date - fix
+            prep.executeUpdate();
+
+            Statement stmt2 = connection.createStatement();
+            String sql = "SELECT * FROM Customer";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //NEED CUSTOMER CLASS FOR BELOW - USING EXAMPLE
+            //while (rs.next()) {
+            //  int id = rs.getInt("id");
+            //String name = rs.getString("name");
+            //int age = rs.getInt("phone");
+            //String address = rs.getString("address");
+            //double salary = rs.getDouble("salary");
+            //java.sql.Date date = rs.getDate("dob");
+            //Employee employee = new Employee(id, name, age, address, salary);
+            //}
+            //rs.close();``
+
+
+
+            connection.close(); //close to free up resources
         } catch (SQLException e) {
             log.error("Database Creation Error",e);
         }
-    }
 
+    }
     /*
     This function will be called when the application shuts down
      */
     public void onStop() {
         System.out.println("Shutting Down...");
+
     }
 
 }
+
