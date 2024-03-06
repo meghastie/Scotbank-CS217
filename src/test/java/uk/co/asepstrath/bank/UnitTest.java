d4a417716b960857129af5ab0d35c4c95c0a5cec
hkhan <hashim.khan.2022@uni.strath.ac.uk>
2024-03-06 13:13:36 +0000

    fixed old test and added some new ones

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -1,9 +1,11 @@
 package uk.co.asepstrath.bank;
 
 import io.jooby.StatusCode;
+import io.jooby.test.MockResponse;
 import io.jooby.test.MockRouter;
 import org.junit.jupiter.api.Test;
 import uk.co.asepstrath.bank.models.Account;
+import uk.co.asepstrath.bank.services.HelperMethods;
 
 import javax.sql.DataSource;
 import java.util.ArrayList;
@@ -16,18 +18,26 @@ public class UnitTest {
     Example can be found in example/UnitTest.java
      */
 
-//    @Test
-//    public void HundredEntries(){
-//        Manager manager = new Manager();
-//        ArrayList<Account> accounts = manager.fetchAccountData();
-//
-//        assertEquals(accounts.size(),100);
-//    }
+    @Test
+    public void HundredEntries(){
+        ArrayList<Account> accounts = HelperMethods.getAccountList();
+
+        assertEquals(accounts.size(),100);
+    }
 
     @Test
     public void accountsWebsiteActive(){
         MockRouter router = new MockRouter(new App());
         router.get("/bank/accounts", rsp -> {
+            assertEquals(StatusCode.OK,rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void mainPageActive(){
+        MockRouter router = new MockRouter(new App());
+
+        router.get("/bank", rsp -> {
             assertEquals(StatusCode.OK,rsp.getStatusCode());
         });
     }
